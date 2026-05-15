e0c75ab0e7a9493509e9d064c080f62b164ac42b
Dmitry Pichuzhkin <pichuzhkin.dmitry@outlook.com>
2026-05-16 02:19:44 +0300

    Server bugs fixes

--- db/index.ts
+++ db/index.ts
@@ -1,3 +1,4 @@
+import 'server-only';
 import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
@@ -6,6 +7,11 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set');
 }
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('neon.tech')
+    ? { rejectUnauthorized: false }
+    : false,
+});
 
 export const db = drizzle(pool, { schema });
