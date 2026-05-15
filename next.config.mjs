896869623cb087bc3f1bf218a521cad7c6ec870f
Dmitry Pichuzhkin <pichuzhkin.dmitry@outlook.com>
2026-05-16 02:40:47 +0300

    Server bugs fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias['@neondatabase/serverless'] = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
