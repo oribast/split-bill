774f4139b939c7fd0a3e46b593ab65d5006c6019
Dmitry Pichuzhkin <pichuzhkin.dmitry@outlook.com>
2026-05-16 23:14:28 +0300

    Main page update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Split Bill",
+  title: "Tally",
   description: "Умный раздел счёта",
 };
 
