c294bdf08be0eae4a90daa507ae19fbc4363fb78
Dmitry Pichuzhkin <pichuzhkin.dmitry@outlook.com>
2026-05-16 19:44:02 +0300

    Deploy fixes

--- app/room/[id]/RoomClient.tsx
+++ app/room/[id]/RoomClient.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import useSWR from "swr";
 import toast from "react-hot-toast";
